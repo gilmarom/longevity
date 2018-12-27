676a5997c01c5d4cb8aa3003f462d13bac6e973d
gilmarom <gilmarom23@gmail.com>
2018-12-27 23:50:19 +0200

    'f'

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const http = require('http');
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/client'));
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'dist/client/index.html' ));
+});
+
+const server = http.createServer(app);
+// Start the app by listening on the default Heroku port
+server.listen(process.env.PORT || 8080);
